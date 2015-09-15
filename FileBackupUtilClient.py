4e3e95fa51566542d49f392b1ad43b4b351e494e
Cody Landry <cody.landry@me.com>
2015-09-14 23:36:58 -0400

    Massive Overhaul of design.  Now using dirsync for directory synchronization.  Also Added GUI elements.  Have 'New' job button working.

--- FileBackupUtilClient.py
+++ FileBackupUtilClient.py
@@ -2,9 +2,60 @@
 from __future__ import print_function
 import Pyro4
 import wx
+import schedule
+import pickle
+from wx.lib.masked import TimeCtrl
+from wx.grid import Grid
+import time
 
-# TODO: Make this a class property
-service = Pyro4.core.Proxy("PYRONAME:service")    # use name server object lookup uri shortcut
+
+def run_task(job):
+    pass
+
+class BackupConfig(wx.Frame):
+    def __init__(self, parent, **kwargs):
+        super(BackupConfig, self).__init__(parent, **kwargs)
+        self.parent = parent
+        self.Size = (640, 480)
+        self.Centre()
+        self.SetTitle('Select Backup Configuration')
+
+        panel = wx.Panel(self)
+
+        # Option 1: Back up on an interval
+
+        self.backup_every_radiobtn = wx.RadioButton(panel, -1, "Backup every:", pos=(10, 44))
+        self.backup_at_radiobtn = wx.RadioButton(panel, -1, "Backup at:", pos=(10, 82))
+        self.backup_every_radiobtn.SetValue(True)
+
+        self.multiplier = wx.SpinCtrl(panel, pos=(435, 42), size=(57, 24))
+        self.multiplier.SetRange(1, 100)
+
+        self.periodicity = wx.Choice(panel, -1, pos=(525, 40), size=(99, 24),
+                                choices=['seconds', 'minutes', 'hours', 'days', 'weeks'])
+
+        self.time_spin_btn = wx.SpinButton(panel, -1, style=wx.SP_VERTICAL, pos=(510, 79))
+        self.time_ = TimeCtrl(panel, -1, pos=(408, 80), spinButton=self.time_spin_btn)
+
+        self.day_of_week = wx.Choice(panel, -1, pos=(525, 77), size=(99, 24),
+                                choices=['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'])
+
+        submit = wx.Button(panel, -1, "Submit", pos=(10, 100))
+
+        submit.Bind(wx.EVT_BUTTON, self.push_to_parent)
+
+        self.Show()
+
+    def push_to_parent(self, event):
+        if self.backup_every_radiobtn.GetValue() == True:
+            self.parent.create_new_job(type_='EVERY',
+                                       multiplier=self.multiplier.GetValue(),
+                                       periodicity=self.periodicity.GetString(self.periodicity.GetSelection()))
+        elif self.backup_at_radiobtn.GetValue() == True:
+            self.parent.create_new_job(type_='AT',
+                                       time_=self.time_.GetValue(),
+                                       day_of_week=self.day_of_week.GetString(self.day_of_week.GetSelection()))
+        self.Close()
 
 
 class WindowClass(wx.Frame):
@@ -14,6 +65,10 @@ class WindowClass(wx.Frame):
     def __init__(self, *args, **kwargs):
         super(WindowClass, self).__init__(*args, **kwargs)
 
+        self.pyro_service = Pyro4.core.Proxy("PYRONAME:pyro_service")    # use name server object lookup uri shortcut
+
+
+        self.Size = (640, 280)
         self.Centre()
         self.SetTitle('File Backup App')
 
@@ -23,7 +78,87 @@ class WindowClass(wx.Frame):
         # --------------Panel Setup-----------------------------
         panel = wx.Panel(self)
 
+        self.table = wx.ListCtrl(panel, -1, pos=(10, 10), size=(611, 199), style=wx.LC_REPORT | wx.LC_HRULES)
+        self.update_table()
+
+        newbtn = wx.Button(panel, -1, "New", pos=(10, 218), size=(65, 24))
+        editbtn = wx.Button(panel, -1, "Edit", pos=(85, 218), size=(65, 24))
+        deletebtn = wx.Button(panel, -1, "Delete", pos=(160, 218), size=(65, 24))
+
+        newbtn.Bind(wx.EVT_BUTTON, self.newbtn_handler)
+
         self.Show()
+
+    def update_table(self):
+        columns = [
+            ("Backup Type", 150),
+            ("Source", 100),
+            ("Destination", 100),
+            ("Last Backup", 200)
+        ]
+        self.tablevalues = self.pyro_service.get_jobs()
+        self.table.ClearAll()
+
+        if len(self.tablevalues):
+            for i, col in enumerate(columns):
+                self.table.InsertColumn(i, col[0], width=col[1])
+            for i, row in enumerate(self.tablevalues):
+                edited_row = []
+                for g, col in enumerate(row[1:]):
+                    if g in [1, 2]:
+                        edited_row.append(str(col)[col.rfind('/'):])
+                    else:
+                        edited_row.append(str(col))
+                self.table.Append(edited_row)
+
+    def show_dirdialog(self, message="Select Folder", defaultPath=""):
+        dirdialog = wx.DirDialog(None, message, defaultPath=defaultPath)
+
+        if dirdialog.ShowModal() == wx.ID_OK:
+            return dirdialog.GetPath()
+
+    def create_new_job(self, *args, **kwargs):
+        newjobcall = None
+        if kwargs['type_'] == 'EVERY':
+            newjobcall = "schedule.Job(" + str(kwargs['multiplier']) + ")." + str(kwargs['periodicity'])
+        elif kwargs['type_'] == 'AT':
+            print(kwargs['time_'])
+            newjobcall = "schedule.Job().at('" + str(kwargs['time_']) + "')"
+
+        if newjobcall:
+            new_job = eval(newjobcall)
+            if kwargs['type_'] == 'AT':
+                new_job.start_day = kwargs['day_of_week'].lower()
+            new_job = pickle.dumps(new_job)
+            self.pyro_service.create_new_job(new_job, self.temp_source, self.temp_target)
+            time.sleep(2)
+            self.update_table()
+
+
+    def newbtn_handler(self, event):
+
+        self.temp_source = self.show_dirdialog(message="Select Source Folder")
+        if not self.temp_source:
+            return
+        time.sleep(1)
+        self.temp_target = self.show_dirdialog(message="Select Target Folder", defaultPath=self.temp_source)
+        if not self.temp_target:
+            return
+
+        BackupConfig(self)
+
+        """
+        interval = 5
+        periodicity = 'seconds'
+
+        test_job = "schedule.Job(" + str(interval) + ")." + periodicity
+        test_job = pickle.dumps(eval(test_job))
+
+        #test_job = pickle.dumps(schedule.Job(20).seconds)
+
+        self.pyro_service.create_new_job(test_job, '/Users/codylandry/PycharmProjects/FileBackupApp/test/from_dir',
+                                         '/Users/codylandry/PycharmProjects/FileBackupApp/test/to_dir')
+        """
 
 
 def main():
--- FileBackupUtilServer.py
+++ FileBackupUtilServer.py
@@ -4,149 +4,121 @@ A Background application for performing
 scheduled backups of new files in user-specified
 locations.
 """
-from collections import namedtuple
+import schedule
 import sqlite3
-import os
-import time
-import shutil
+import pickle
+import datetime as dt
 import socket
 import select
 import sys
 import Pyro4.core
 import Pyro4.naming
-
-
-Job = namedtuple('Job', ['job_id', 'interval', 'from_location', 'to_location', 'state'])
+import dirsync
+from collections import namedtuple
 
 
 class PyroService():
     def __init__(self):
         self.db = None
         self.dbconn = None
-        self.jobs = []
         self.connect_to_db()
-        self.get_db_jobs()
+        self.schedule = schedule.Scheduler()
+
+    def job_last_run(self, job):
+        def format_time(t):
+            return t.strftime('%Y-%m-%d %H:%M:%S') if t else '[never]'
+
+        return '(last run: %s, next run: %s)' % (
+                    format_time(job.last_run), format_time(job.next_run))
+
+    def job_desc(self, job):
+        if job.at_time is not None:
+            return 'Every %s %s at %s' % (job.interval, job.unit[:-1] if job.interval == 1 else job.unit, job.at_time)
+        else:
+            return 'Every %s %s' % (job.interval, job.unit[:-1] if job.interval == 1 else job.unit)
 
     def connect_to_db(self):
         global dbconn, db
         try:
             self.db.close()
+            self.db = None
+            self.dbconn = None
         except:
             pass
         self.dbconn = sqlite3.connect('filehist.db')
         self.db = self.dbconn.cursor()
-        self.db.execute('CREATE TABLE IF NOT EXISTS FileHistory ('
+        self.db.execute('CREATE TABLE IF NOT EXISTS jobs ('
                         'job_id INTEGER PRIMARY KEY AUTOINCREMENT ,'
-                        'interval INTEGER NOT NULL,'
+                        'job INTEGER NOT NULL,'
                         'from_location TEXT NOT NULL,'
                         'to_location TEXT NOT NULL,'
-                        'state BLOB NOT NULL)')
+                        'last_backup TEXT)')
 
-    @staticmethod
-    def get_dir_timestamps(directory):
-        # Returns a dictionary of filename, timestamp pairs from the directory given
-        hist = dict()
-        for file in os.listdir(directory):
-            hist[file] = os.path.getmtime(directory + '/' + file)
-        return hist
-
-    def get_db_jobs(self):
+    def reload_tasks_from_db(self):
         # Gets all jobs
         self.connect_to_db()
-        self.db.execute('SELECT * FROM FileHistory')
-        for job in self.db.fetchall():
-            self.jobs.append(Job(*job))
+        self.db.execute('SELECT * FROM jobs')
+        jobs = []
+        for row in list(self.db.fetchall()):
+            job = pickle.loads(row[1])
+            print self.job_desc(job)
+            job.do(self.run_task, row[2], row[3])
+            jobs.append(job)
+        self.schedule.jobs = jobs
 
-    def get_db_job(self, job_id):
-        if self.jobs:
-            for job in self.jobs:
-                if job.job_id == job_id:
-                    return job
+    def get_jobs(self):
+        Job = namedtuple("Job", ('job_id', 'job_desc', 'from_location', 'to_location', 'last_backup'))
+        self.connect_to_db()
+        self.db.execute('SELECT job_id, job, from_location, to_location, last_backup FROM jobs')
+        ret = []
+        for row in list(self.db.fetchall()):
+            row = list(row)
+            row[1] = self.job_desc(pickle.loads(row[1]))
+            ret.append(Job(*row))
+        print 'get_jobs called'
+        return ret
 
     @Pyro4.expose
-    def update_job(self, **kwargs):
+    def update_job(self, job_id, job, from_location, to_location):
         self.connect_to_db()
+        job_obj = pickle.loads(job)
+        job_obj.do(self.run_task, from_location, to_location)
+        self.db.execute('UPDATE jobs SET job = "{}", from_location = "{}", to_location = "{}" '
+                        'WHERE job_id == {}'.format(job, from_location, to_location, job_id))
+        self.dbconn.commit()
+        self.reload_tasks_from_db()
 
+    @Pyro4.oneway
     @Pyro4.expose
-    def create_new_job(self, interval, from_location, to_location):
+    def create_new_job(self, job, from_location, to_location):
         self.connect_to_db()
-        state = self.get_dir_timestamps(from_location)
-        try:
-            self.db.execute('INSERT INTO filehistory (interval, from_location, to_location, state)'
-                            'VALUES ("{}", "{}", "{}", "{}")'.format(interval, from_location, to_location, state))
-            self.dbconn.commit()
-            self.get_db_jobs()
+        job_obj = pickle.loads(job)
+        job_obj.do(self.run_task, from_location, to_location)
+        print self.job_desc(job_obj)
+        print from_location, to_location
+        self.db.execute('INSERT INTO jobs (job, from_location, to_location)'
+                        'VALUES ("{}", "{}", "{}")'.format(job, from_location, to_location))
+        self.dbconn.commit()
+        self.reload_tasks_from_db()
 
-            return True
-        except Exception as e:
-            print 'Exception in Function: create_new_job;', e
-            return False
 
-    # TODO: Add update_job()
     # TODO: Add delete_job()
 
-    def get_files_to_backup(self, job):
-        files_to_backup = []
-        current_state = self.get_dir_timestamps(job.from_location)
-        try:
-            # loop through each file in the job
-            for file, timestamp in current_state.items():
-                # get the modified times of each file in the 'to_location' directory
-                to_location_state = self.get_dir_timestamps(job.to_location)
-                # if the modified times aren't equal and it's been more than the interval period, backup the file
-                if (current_state[file] != to_location_state.get(file, -1)) and \
-                        (time.time() > current_state[file] + job.interval) and \
-                        (os.path.isdir('/'.join([job.from_location, file])) is False):
-                    files_to_backup.append(file)
-            return files_to_backup
-        except Exception as e:
-            # User may delete files from the 'to location'
-            print 'Exception in get_files_to_backup:', e
-            return None
-
-    def backup_file(self, job, file):
-        # shutil copies the file and most of the metadata including the modified time
-        from_path = '/'.join([job.from_location, file])
-        to_path = '/'.join([job.to_location, file])
-
-        if not os.path.isdir(from_path):
-            shutil.copy2(from_path, to_path)
-            print 'Backed Up File:', file, 'to:', job.to_location
-
-    def update_dir_state(self, job):
-        # Update the database 'state' of the 'from_location' for display to the user
+    def run_task(self, from_location, to_location):
         self.connect_to_db()
-        self.db.execute('UPDATE FileHistory '
-                        'SET state = "{}" WHERE job_id == "{}"'.format(self.get_dir_timestamps(job.from_location), job.job_id))
+        timestamp = dt.datetime.now()
+        self.db.execute('UPDATE jobs SET last_backup = "{}" '
+                        'WHERE from_location == "{}" AND to_location == "{}"'.format(timestamp,
+                                                                                     from_location, to_location))
         self.dbconn.commit()
+        dirsync.sync(from_location, to_location, action='sync', verbose=True)
 
-    def run_jobs(self):
-        update = False
-        for job in self.jobs:
-            files = self.get_files_to_backup(job)
-            if files:
-                for f in files:
-                    update = True
-                    self.backup_file(job, f)
-                if update:
-                    self.update_dir_state(job)
-                    update = False
-
-    def backup_now(self, job_id):
-        # Initiates a backup of all files
-        globals()
-        self.get_db_jobs()
-        job = self.get_db_job(job_id)
-        for f in eval(job.state).keys():
-            self.backup_file(job, f)
-        self.connect_to_db()
-        self.update_dir_state(job)
+pyro_service = PyroService()
+pyro_service.connect_to_db()
+pyro_service.reload_tasks_from_db()
 
 
-service = PyroService()
-service.connect_to_db()
-service.run_jobs()
-
+# <editor-fold desc="Pyro Server Setup">
 if sys.version_info < (3,0):
     input = raw_input
 
@@ -171,13 +143,14 @@ print("daemon location string=%s" % pyrodaemon.locationStr)
 print("daemon sockets=%s" % pyrodaemon.sockets)
 
 # register a server object with the daemon
-serveruri=pyrodaemon.register(service)
+serveruri=pyrodaemon.register(pyro_service)
 print("server uri=%s" % serveruri)
 
 # register it with the embedded nameserver directly
-nameserverDaemon.nameserver.register("service", serveruri)
+nameserverDaemon.nameserver.register("pyro_service", serveruri)
 
 print("")
+# </editor-fold>
 
 while True:
     # create sets of the socket objects we will be waiting on
@@ -205,7 +178,7 @@ while True:
         print("Daemon received a request")
         pyrodaemon.events(eventsForDaemon)
 
-    service.run_jobs()
+    pyro_service.schedule.run_pending()
 
 
 nameserverDaemon.close()
